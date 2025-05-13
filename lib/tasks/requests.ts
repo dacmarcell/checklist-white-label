29fa92a4977dcce93c8b8c5880da4b0a36de33d4
devmarcell <devmarcell@protonmail.com>
2025-05-13 18:39:37 -0300

    Add fetchTasks function to retrieve finished tasks from the API

---
+++ lib/tasks/requests.ts
@@ -0,0 +1,10 @@
+export const fetchTasks = async () => {
+  const response = await fetch("/api/tasks/finished");
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message);
+  }
+
+  return data.message;
+};
