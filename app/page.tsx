300496a04a0c6ef1fd1863406d7249830626ee75
devmarcell <devmarcell@protonmail.com>
2025-05-18 13:48:50 -0300

    Refactor Home component to use tenantConfig directly for logo and details

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,23 @@
 "use client";
 
-import { app } from "@/lib/app";
-import { getTenantConfig } from "@/lib/tenants";
+import { tenantConfig } from "@/lib/app";
 import Image from "next/image";
 
 export default function Home() {
-  const tenant = getTenantConfig(app.currentTenant);
-
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src={tenant.logo}
+          src={tenantConfig.logo}
           alt="Next.js logo"
           width={180}
           height={38}
           priority
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">{tenant.name}</li>
-          <li className="tracking-[-.01em]">{tenant.description}</li>
+          <li className="mb-2 tracking-[-.01em]">{tenantConfig.name}</li>
+          <li className="tracking-[-.01em]">{tenantConfig.description}</li>
         </ol>
       </main>
     </div>
