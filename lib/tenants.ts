1ae0e9d62d2ad63a727297d1cc0cb7dbb07d2624
devmarcell <devmarcell@protonmail.com>
2025-06-10 22:52:36 -0300

    Refactor tenants colors structure for simplified configuration

--- lib/tenants.ts
+++ lib/tenants.ts
@@ -11,18 +11,10 @@ export const tenants = {
     description: "Default tenant",
     logo: "/next.svg",
     colors: {
-      primary: {
-        light: "gray-400",
-        dark: "gray-400",
-      },
-      secoundary: {
-        light: "gray-400",
-        dark: "gray-400",
-      },
-      background: {
-        light: "white",
-        dark: "gray-900",
-      },
+      primary: "gray-400",
+      text: "white",
+      secoundary: "gray-200",
+      background: "gray-800",
     },
   },
   dacti: {
@@ -30,18 +22,10 @@ export const tenants = {
     description: "Dacti tenant",
     logo: "/file.svg",
     colors: {
-      primary: {
-        light: "gray-400",
-        dark: "gray-400",
-      },
-      secoundary: {
-        light: "gray-400",
-        dark: "gray-400",
-      },
-      background: {
-        light: "white",
-        dark: "gray-900",
-      },
+      primary: "gray-400",
+      text: "white",
+      secoundary: "gray-200",
+      background: "gray-900",
     },
   },
 };
